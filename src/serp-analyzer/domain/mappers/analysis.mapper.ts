9960a0748ab77af734db1d294792ff6d85fdd131
Przemyslaw Chudzinski <p.chudzinski.spectreit@gmail.com>
2025-04-07 14:48:13 +0200

    WIP: SST2-160

---
+++ src/serp-analyzer/domain/mappers/analysis.mapper.ts
@@ -0,0 +1,23 @@
+import { SaAnalysis } from '@prisma/client';
+import { Analysis } from '../analysis';
+
+export class AnalysisMapper {
+  static toDomain(
+    model: SaAnalysis,
+    hasActiveTestingMode: boolean,
+    hasActiveSubscription: boolean,
+    exceededMonthlyLimit: boolean,
+  ) {
+    return new Analysis(
+      model.id,
+      model.keyword,
+      model.localizationId,
+      model.processId,
+      model.device,
+      model.userId,
+      hasActiveTestingMode,
+      hasActiveSubscription,
+      exceededMonthlyLimit,
+    );
+  }
+}
--- src/serp-analyzer/infrastructure/persistence/prisma-analysis.repository.ts
+++ src/serp-analyzer/infrastructure/persistence/prisma-analysis.repository.ts
@@ -2,22 +2,138 @@ import { Injectable } from '@nestjs/common';
 import { AnalysisRepository } from '../../application/ports/analysis.repository';
 import { DatabaseService } from '../../../database/database.service';
 import { Analysis } from '../../domain/analysis';
-
-// todo;
+import * as moment from 'moment';
+import { AnalysisMapper } from '../../domain/mappers/analysis.mapper';
 
 @Injectable()
 export class PrismaAnalysisRepository implements AnalysisRepository {
   constructor(private readonly databaseService: DatabaseService) {}
 
   async save(analysis: Analysis): Promise<void> {
-    return Promise.resolve(undefined);
+    const analysisModel = await this.databaseService.saAnalysis.findUnique({
+      where: {
+        id: analysis.getAnalysisId(),
+      },
+    });
+
+    if (analysisModel) {
+      await this.databaseService.saAnalysis.update({
+        where: {
+          id: analysis.getAnalysisId(),
+        },
+        data: {
+          device: analysis.getDevice(),
+          keyword: analysis.getKeyword(),
+          localizationId: analysis.getLocalizationId(),
+          processId: analysis.getProcessId(),
+          userId: analysis.getUserId(),
+        },
+      });
+      return;
+    }
+
+    await this.databaseService.saAnalysis.create({
+      data: {
+        id: analysis.getAnalysisId(),
+        device: analysis.getDevice(),
+        keyword: analysis.getKeyword(),
+        localizationId: analysis.getLocalizationId(),
+        processId: analysis.getProcessId(),
+        createdAt: moment().unix(),
+        userId: analysis.getUserId(),
+      },
+    });
   }
 
-  findById(analysisId: string): Promise<Analysis> {
-    return Promise.resolve(undefined);
+  async findById(analysisId: string): Promise<Analysis> {
+    const model = await this.databaseService.saAnalysis.findUnique({
+      where: { id: analysisId },
+    });
+    if (!model) return null;
+
+    const testingModeModel =
+      await this.databaseService.saTestingMode.findUnique({
+        where: { userId: model.userId },
+      });
+
+    const userSubscriptionInfoModel =
+      await this.databaseService.saUserSubscriptionInfo.findUnique({
+        where: {
+          userId: model.userId,
+        },
+      });
+
+    const hasActiveTestingMode = testingModeModel
+      ? testingModeModel.active
+      : false;
+    const hasActiveSubscription = userSubscriptionInfoModel
+      ? userSubscriptionInfoModel.active
+      : false;
+    const exceededMonthlyLimit = await this.exceededMonthlyLimit(model.userId);
+
+    return AnalysisMapper.toDomain(
+      model,
+      hasActiveTestingMode,
+      hasActiveSubscription,
+      exceededMonthlyLimit,
+    );
   }
 
-  exceededMonthlyLimit(userId: string): Promise<boolean> {
-    return Promise.resolve(false);
+  async exceededMonthlyLimit(userId: string): Promise<boolean> {
+    const testingModeModel =
+      await this.databaseService.saTestingMode.findUnique({
+        where: { userId: userId },
+      });
+
+    const userSubscriptionInfoModel =
+      await this.databaseService.saUserSubscriptionInfo.findUnique({
+        where: {
+          userId: userId,
+        },
+      });
+
+    const nowMoment = moment();
+
+    const fromDateMoment = nowMoment.clone().set({
+      M: nowMoment.get('month'),
+      D: 1,
+      h: 0,
+      minute: 0,
+      second: 0,
+    });
+
+    const toDateMoment = nowMoment.clone().set({
+      h: 23,
+      minute: 59,
+      second: 59,
+      M: nowMoment.get('month'),
+      D: nowMoment.daysInMonth(),
+    });
+
+    const total = await this.databaseService.saAnalysis.count({
+      where: {
+        userId: userId,
+        createdAt: {
+          gte: fromDateMoment.unix(),
+          lte: toDateMoment.unix(),
+        },
+      },
+    });
+
+    if (
+      userSubscriptionInfoModel &&
+      userSubscriptionInfoModel.active &&
+      total >= userSubscriptionInfoModel.analysisPerMonth
+    )
+      return true;
+
+    if (
+      testingModeModel &&
+      testingModeModel.active &&
+      total >= testingModeModel.analysisPerMonth
+    )
+      return true;
+
+    return true;
   }
 }
